4552f9afc473eec6f2aed02e3faf9de32a9a978c
4rturd13 <4rturd13@gmail.com>
2020-11-25 18:46:40 -0500

    edit class responsive

--- src/js/index.js
+++ src/js/index.js
@@ -20,45 +20,50 @@ addEventListener('DOMContentLoaded', () => {
   const observer = new IntersectionObserver(functionObserver, {
     root: null,
     rootMargin: '0px',
-    threshold: 0.8,
+    threshold: 0.5,
   })
 
   sections.forEach((section) => observer.observe(section))
-
-  const getData = () => {
-    const sectionOne = document.querySelector('.section-one')
-    fetch('data.json')
-      .then((res) => res.json())
-      .then((data) => {
-        let sections = data.data
-        let count = 0
-        let acum = ''
-        sections
-          .forEach((section) => {
-            sectionOne.innerHTML += `<img
-          class="image-section"
-          src="${section.sectionImage}"
-          alt="${section.name}"
-        />`
-            section.item.forEach((obj) => {
-              count++
-              let title = obj.title
-              let description = obj.description
-              let price = obj.price
-              return (acum += `
-              <article class="item">
-                <h6 class="item-title">${title}</h6>
-                <p class="item-description">
-                  ${description}
-                </p>
-                <p class="item-price">$${price}</p>
-              </article>
-            `)
-            })
-            sectionOne.innerHTML += acum
-          })
-          .catch((error) => error)
-      })
-  }
-  getData()
 })
+
+// const getData = () => {
+//   fetch('data.json')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       let sections = data.data
+//       let count = 0
+//       let acum = ''
+//       let content = ''
+
+//       sections
+//         .forEach((section) => {
+//           const sectionOne = document.querySelector('.main-content')
+//           content += `<img
+//             class="image-section"
+//             src="${section.sectionImage}"
+//             alt="${section.name}"
+//           />`
+
+//           section.item.forEach((obj) => {
+//             count++
+//             let title = obj.title
+//             let description = obj.description
+//             let price = obj.price
+//             return (acum += `
+//             <article class="item">
+//               <h6 class="item-title">${title}</h6>
+//               <p class="item-description">
+//                 ${description}
+//               </p>
+//               <p class="item-price">$${price}</p>
+//             </article>
+//           `)
+//           })
+//           console.log(content)
+//           content += acum
+//           sectionOne.innerHTML = content
+//         })
+//         .catch((error) => error)
+//     })
+// }
+// getData()
